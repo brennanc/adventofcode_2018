3aa6c5cf502ffbe1ceebe7c78dd14c4704540cd5
Brennan Cropper <bcropper@gmail.com>
2018-12-04 20:38:58 -0600

    day 1b

--- kotlin/src/test/Day1Test.kt
+++ kotlin/src/test/Day1Test.kt
@@ -5,15 +5,30 @@ import org.junit.Test as test
 
 internal class Day1Test {
     private val day1 = Day1()
+    val input = File("src/test/resources/day1.txt").useLines { it.toList() }
 
     @test
     fun day1a() {
-        val input = File("src/test/resources/day1.txt").useLines { it.toList() }
         val num = input.map { it.toInt() }.sum()
         println(num)
     }
 
     @test
     fun day1b() {
+        var freqs : MutableSet<Int> = mutableSetOf()
+        var freq = 0
+        var repeatedFreq : Int? = null;
+        while (repeatedFreq == null) {
+            for (i in input) {
+                freq += i.toInt()
+                if (freqs.contains(freq)) {
+                    repeatedFreq = freq
+                    break
+                } else {
+                    freqs.add(freq)
+                }
+            }
+        }
+        println(repeatedFreq)
     }
 }
